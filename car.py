7ff3ed3530760ea0df9966c677e0299138d1ed12
Lehan Max <lehanmx14@gmail.com>
2019-07-24 10:26:58 +0530

    python D-3

--- car.py
+++ car.py
@@ -3,19 +3,38 @@ class Car:
     def __init__(self, regno, no_gears):
         self.regno = regno
         self.no_gears = no_gears
+        self.is_started = False
 
     def start(self):
-        print(f"{self.regno} started....")
+        if self.is_started:
+            print(f"car with reg_no: {self.regno} is already started")
+        else:
+            print(f"car with reg_no: {self.regno} started....")
+            self.is_started = True
     
     def stop(self):
-        print(f"{self.regno} stopped....")
-    
-    def change_gear(self):
-        print(f"{self.regno} changed gear....")
+        if self.is_started:
+            print(f"car with reg_no: {self.regno} stopped....")
+            self.is_started = False
+        else:
+            print(f"car with reg_no: {self.regno} has already stopped....")
+            
 
+    def change_gear(self):
+        if self.is_started:
+            print(f"car with reg_no: {self.regno} changed gear....")
+        else:
+            print(f"car with reg_no: {self.regno} has already stopped... can't change gear....")
     
 if __name__ == "__main__":
     bmw = Car("KA01300",5)
+    audi = Car("KA450067",6)
     bmw.start()
     bmw.change_gear()
     bmw.stop()
+    bmw.stop()
+    bmw.change_gear()
+
+    audi.start()
+    audi.stop()
+    audi.change_gear()
